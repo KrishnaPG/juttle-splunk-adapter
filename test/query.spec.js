d07483e4324c2d94802e9f7e75f1b20c233f63c7
Balazs Kutil <balazs@jut.io>
2016-03-10 22:36:51 +0100

    query.js - use static filter compiler instead of ast-visitor

--- test/query.spec.js
+++ test/query.spec.js
@@ -12,55 +12,55 @@ withAdapterAPI(() => {
     describe('splunk query building', () => {
         it('basic query', () => {
             var filter_ast = utils.parseFilter('index = "cpu"');
-            expect(builder.build(filter_ast)).to.equal('search index = "cpu" | sort + _time');
+            expect(builder.compile(filter_ast)).to.equal('search index = "cpu" | sort + _time');
         });
 
         it('implicit AND', () => {
             var filter_ast = utils.parseFilter('name = "cpu1" index="cpu"');
-            expect(builder.build(filter_ast)).to.equal('search name = "cpu1" AND index = "cpu" | sort + _time');
+            expect(builder.compile(filter_ast)).to.equal('search name = "cpu1" AND index = "cpu" | sort + _time');
         });
 
         it('OR', () => {
             var filter_ast = utils.parseFilter('name = "cpu1" or index="cpu"');
-            expect(builder.build(filter_ast)).to.equal('search name = "cpu1" OR index = "cpu" | sort + _time');
+            expect(builder.compile(filter_ast)).to.equal('search name = "cpu1" OR index = "cpu" | sort + _time');
         });
 
         it('nested binary expressions', () => {
             var filter_ast = utils.parseFilter('(name = "cpu1" or index="cpu") and host = "host1"');
-            expect(builder.build(filter_ast)).to.equal('search (name = "cpu1" OR index = "cpu") AND host = "host1" | sort + _time');
+            expect(builder.compile(filter_ast)).to.equal('search (name = "cpu1" OR index = "cpu") AND host = "host1" | sort + _time');
         });
 
         it('missing index throws', () => {
             var filter_ast = utils.parseFilter('name = "cpu"');
-            expect(builder.build.bind(builder, filter_ast)).to.throw(/Missing required field "index"/);
+            expect(builder.compile.bind(builder, filter_ast)).to.throw(/Missing required field "index"/);
         });
 
         it('=~ throws', () => {
             var filter_ast = utils.parseFilter('name =~ "cpu"');
-            expect(builder.build.bind(builder, filter_ast)).to.throw(/Operator =~ not supported/);
+            expect(builder.compile.bind(builder, filter_ast)).to.throw(/Operator =~ not supported/);
         });
 
         it('!~ throws', () => {
             var filter_ast = utils.parseFilter('name !~ "cpu"');
-            expect(builder.build.bind(builder, filter_ast)).to.throw(/Operator !~ not supported/);
+            expect(builder.compile.bind(builder, filter_ast)).to.throw(/Operator !~ not supported/);
         });
 
         it('in throws', () => {
             var filter_ast = utils.parseFilter('name in ["cpu","memory"]');
-            expect(builder.build.bind(builder, filter_ast)).to.throw(/Operator in not supported/);
+            expect(builder.compile.bind(builder, filter_ast)).to.throw(/Operator in not supported/);
         });
 
         describe('optimizations', () => {
             it('head', () => {
                 var filter_ast = utils.parseFilter('index = "cpu"');
                 var opt_info = { head: { limit: 10 } };
-                expect(builder.build(filter_ast, { optimizations: opt_info })).to.equal('search index = "cpu" | sort + _time | head 10');
+                expect(builder.compile(filter_ast, { optimizations: opt_info })).to.equal('search index = "cpu" | sort + _time | head 10');
             });
 
             it('tail', () => {
                 var filter_ast = utils.parseFilter('index = "cpu"');
                 var opt_info = { tail: { limit: 10 } };
-                expect(builder.build(filter_ast, { optimizations: opt_info })).to.equal('search index = "cpu" | sort + _time | tail 10');
+                expect(builder.compile(filter_ast, { optimizations: opt_info })).to.equal('search index = "cpu" | sort + _time | tail 10');
             });
         });
     });
